557f1e02147b7285c7377950e0535e169cec313a
thyoondev <th.yoon.dev@gmail.com>
2022-01-28 18:30:56 +0900

    마우스 위치에 따라 시점 변경

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,7 +1,9 @@
 (function () {
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  const stageElem = document.querySelector(".stage");
+  const mousePos = { x: 0, y: 0 };
+  let maxScrollValue;
 
   function resizeHandler() {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
@@ -16,5 +18,12 @@
     barElem.style.width = scrollPer * 100 + "%";
   });
 
+  window.addEventListener("mousemove", function (e) {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = "rotateX( " + mousePos.y * 5 + "deg) rotateY(" + mousePos.x * 5 + "deg)";
+  });
+
   window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
