aa5984a5812e76932c5fc3c1db36ac7d1da78dc4
thyoondev <th.yoon.dev@gmail.com>
2022-01-28 18:15:06 +0900

    progress bar

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,5 +1,6 @@
 (function () {
   const houseElem = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue = document.body.offsetHeight - window.innerHeight;
 
   function resizeHandler() {
@@ -7,8 +8,12 @@
   }
 
   window.addEventListener("scroll", function () {
-    const zMove = (scrollY / maxScrollValue) * 980 - 490; //스크롤 비율
+    const scrollPer = scrollY / maxScrollValue;
+    const zMove = scrollPer * 980 - 490; //스크롤 비율
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
+
+    // progress bar
+    barElem.style.width = scrollPer * 100 + "%";
   });
 
   window.addEventListener("resize", resizeHandler);
