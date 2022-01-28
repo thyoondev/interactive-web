5136b457b7708376d67ee3ad96d22e1b0032997b
thyoondev <th.yoon.dev@gmail.com>
2022-01-28 16:43:03 +0900

    resize handler

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,9 +1,15 @@
 (function () {
   const houseElem = document.querySelector(".house");
-  const maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener("scroll", function () {
     const zMove = (scrollY / maxScrollValue) * 980 - 490; //스크롤 비율
     houseElem.style.transform = "translateZ(" + zMove + "vw)";
   });
+
+  window.addEventListener("resize", resizeHandler);
 })();
